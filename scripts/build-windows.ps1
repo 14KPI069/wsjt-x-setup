578a2b2dafc86c42cce9d875be0f5a293a682db4
14KPI069 <billardpascal626@gmail.com>
2026-09-14 17:42:24 +0200

    Update target script path in build-windows.ps1

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -37,8 +37,8 @@ $env:CHERE_INVOKING = '1'
 
 $u = (& $bash -lc "cygpath -u '$hereFwd'").Trim()
 $forward = ($args -join ' ')
-$targetScript = Join-Path $here "build-windows-cross.sh"
+$targetScript = Join-Path $PSScriptRoot "build-windows-cross.sh"
 (Get-Content -Raw $targetScript) -replace "`r`n", "`n" | Set-Content -NoNewline $targetScript
 Write-Host "Running build-windows-cross.sh in MSYS2 UCRT64 ($msys)..." -ForegroundColor Cyan
-& $bash -lc "'$u/build-windows-cross.sh' $forward"
+& $bash -lc "'/d/a/wsjt-x-setup/wsjt-x-setup/scripts/build-windows-cross.sh' $forward"
 exit $LASTEXITCODE
